929466dfd953624feb1a4051144dd617f7ce5ec3
EC2 Default User <ec2-user@ip-172-31-31-36.eu-central-1.compute.internal>
2018-03-29 13:21:57 +0000

    Reworked header drop

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+DEBUG = True
+
 rates_url = 'https://www.generali.hu/Ugyfelszolgalat/Informaciok/Befektetesek/Eszkozalapjaink.aspx'
 
 def scraper(url):
@@ -17,24 +19,22 @@ def parse_table_bs(html):
     values_table = []
     soup = BeautifulSoup(html, 'html.parser')
     for table in soup.findAll("table", { "class" : "generali-table" }):
-        # Drop first row of table header
-        first = True
         for row in table.findAll("tr"):
-            if first:
-                first = False
-                continue
             cells = row.findAll("td")
             if len(cells) != 3:
                 raise Exception('Column number mismatch')
             values_table_row = [elem.text.strip() for elem in cells]
             values_table.append(values_table_row)
+        # Drop table header
+        values_table = values_table[1:]
     return values_table
 
 def main():
     db_connector("mysql")
     raw_data = scraper(rates_url)
     vt = parse_table_bs(raw_data)
-    print vt
-
+    if DEBUG:
+        for item in vt:
+            print item
 if __name__ == "__main__":
     main()
