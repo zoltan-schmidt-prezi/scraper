0885a689e76c55122dd71d33e09a3f20d9c1716d
EC2 Default User <ec2-user@ip-172-31-31-36.eu-central-1.compute.internal>
2018-04-17 19:34:17 +0000

    Altered scraper_preprod table
    removed name,currency from main_exchange
    added currency to selector

---
+++ scripts/update_curr.sql
@@ -0,0 +1,10 @@
+update selector set currency="EUR" where id=53780421;
+update selector set currency="EUR" where id=80231445; 
+update selector set currency="EUR" where id=70726597;
+update selector set currency="EUR" where id=22277356;
+update selector set currency="EUR" where id=90994319; 
+update selector set currency="EUR" where id=41503527; 
+update selector set currency="EUR" where id=99907552;
+update selector set currency="EUR" where id=5431937;
+
+
